3b4594e64ee0ac39cd0e631cea14573186bf14b2
rakesharakeshaalagawadi-git <rakesharakeshaalagawadi@gmail.com>
2025-11-28 11:09:33 +0530

    Refactor calculate_speed function and add example call

--- speed.py
+++ speed.py
@@ -1,12 +1,5 @@
-# speed.py
+def calculate_speed(distance, time):
+    speed =distance / time
+    return speed
 
-def calculate_speed(d, t):
-    if t == 0:
-        return "Time cannot be zero"
-    return d / t
-
-# Optional manual run
-if _name_ == "_main_":
-    d = float(input("Enter distance (d): "))
-    t = float(input("Enter time (t): "))
-    print("Speed =", calculate_speed(d, t))
+print(calculate_speed(100, 2))
